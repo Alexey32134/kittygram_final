72c83e622b181fb7127801a6005d5d88b824ed7b
Alexey32134 <Alexey32134@yandex.ru>
2024-04-20 00:52:51 +0700

    ke

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -15,7 +15,7 @@ if os.getenv('DEBUG', default='False').lower == 'true':
     DEBUG = True
 DEBUG = False
 
-ALLOWED_HOSTS = str(os.getenv('ALLOWED_HOSTS'), default='kittygram.space').split()
+ALLOWED_HOSTS = str(os.getenv('ALLOWED_HOSTS', default='kittygram.space')).split()
 #ALLOWED_HOSTS = ['51.250.104.145', '127.0.0.1', 'localhost', 'kittygramyp.space']
 
 INSTALLED_APPS = [
