75e8fa1ca6a8fbcd566e6a1e2d0707162fd282c0
Alexey32134 <Alexey32134@yandex.ru>
2024-04-19 21:52:19 +0700

    Check ts

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -15,7 +15,8 @@ if os.getenv('DEBUG', default='False').lower == 'true':
     DEBUG = True
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.0 localhost').split()
+#ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1 localhost').split()
+ALLOWED_HOSTS = ['51.250.104.145', '127.0.0.1', 'localhost', 'kittygramyp.space']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
